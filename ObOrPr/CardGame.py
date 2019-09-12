9ca726d813310d5f7b9fa67df8c438e5bffd2bcd
Ultrabot <ultrabot1294@outlook.com>
2019-09-12 09:44:09 +0100

    Card Game Start

---
+++ ObOrPr/CardGame.py
@@ -0,0 +1,29 @@
+#Adam Baker
+#Card Game
+
+class Card:
+    #__init__ command creates the object attributes for the Card (suit, val)
+    #when the variable is init-ialised
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+    def showCard(self):
+        print("{} of {}".format(self.value, self.suit))
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        self.build()
+    def build(self):
+        for s in ["Spades","Clubs","Diamonds","Hearts"]:
+            for v in range(1,14):
+                self.cards.append(Card(s,v))
+    def show(self):
+        for c in self.cards:
+            c.showCard()
+class Player:
+
+    def __init__(self):
+        pass
+deck = Deck()
+deck.show()
