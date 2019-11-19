10c0740fdf2dc0fa08f724342a96ac3c9b9dcd77
Ultrabot <ultrabot1294@outlook.com>
2019-11-19 10:53:37 +0000

    Stack update

--- stack.py
+++ stack.py
@@ -8,8 +8,19 @@ class Stack:
     def pop(self):
         return self.items.pop()
     def peek(self):
-        return self.item[len(self.items)-1]
+        return self.items[len(self.items)-1]
     def size(self):
         return len(self.items)
 
 s=Stack()
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
