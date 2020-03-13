58fc738ce2ec97dc735c2daa8d87f80574762f95
Ultrabot <ultrabot1294@outlook.com>
2020-03-13 08:54:24 +0000

    Sorting Update

--- strangesort.py
+++ strangesort.py
@@ -1,13 +1,12 @@
-alist = [23,121,11,9,69,420,42,1,12,47]
+alist = [23,121,11,9,69,420,42,11,1,12,47]
 sorted = []
 while len(alist) > 0:
     for item in alist:
         count = 0
         for item2 in alist:
-            if not item == item2:
-                if item < item2:
-                    count += 1
-        if count == (len(alist)-1):
+            if item <= item2:
+                count += 1
+        if count == (len(alist)):
             alist.remove(item)
             sorted.append(item)
 print(sorted)
