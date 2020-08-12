e9a2912408ae5a9b5bfaa6cc7691c2972d8313d3
Ultrabot <ultrabot1294@outlook.com>
2020-08-12 17:30:56 +0100

    More NEA code

--- NEA_Code/main.py
+++ NEA_Code/main.py
@@ -3,11 +3,16 @@ pygame.init()
 
 screenWidth = (800)
 screenHeight = (600)
+mouseForce = 15
+scale = 10
+fonty = pygame.font.Font('freesansbold.ttf',20)
 
 class Instance():
     #Main class for all objects within the simulation
     global xM
     global yM
+    global mouseForce
+    global scale
     def __init__(self,name="New Object",colour=[255,255,255],density=10,xpos=0,ypos=0,mass=10,shape="blank",size=1,xLength=1,yLength=1,thickness=1,youngs=1000,refractive=99999999,roughness=99999999):
         #Defines the attributes for the object
         self.frozen = {
@@ -52,24 +57,30 @@ class Instance():
         self.selected = False
         self.drag = 0.98
         self.elasticity = 0.75
+        self.xforce = 0
+        self.yforce = 0
     def recalibrate(self):
         #Method for resetting variables (unless frozen)
         for attribute in self.frozen:
             if not self.frozen[attribute]:
                 if attribute == "xLocation":
                     if not self.selected:
-                        self.xLocation += ((self.xVelocity+self.xVelocityDelayed)/2)/12
+                        self.xLocation += (((self.xVelocity+self.xVelocityDelayed)/2)/120)*scale
                     else:
                         dx = event.pos[0] - self.xLocation
+                        self.xVelocityDelayed = self.xVelocity
                         self.xVelocity = xM*0.5
-                        self.xLocation += ((self.xVelocity+self.xVelocityDelayed)/2)/1.2
+                        self.xLocation += ((((self.xVelocity+self.xVelocityDelayed)/2)/120))*mouseForce
+                        self.xforce = self.mass * self.xVelocityDelayed - self.xVelocity
                 elif attribute == "yLocation":
                     if not self.selected:
-                        self.yLocation += ((self.yVelocity+self.yVelocityDelayed)/2)/12
+                        self.yLocation += (((self.yVelocity+self.yVelocityDelayed)/2)/120)*scale
                     else:
                         dy = event.pos[1] - self.yLocation
+                        self.yVelocityDelayed = self.yVelocityDelayed
                         self.yVelocity = yM*0.5
-                        self.yLocation += ((self.yVelocity+self.yVelocityDelayed)/2)/1.2
+                        self.yLocation += ((((self.yVelocity+self.yVelocityDelayed)/2)/120))*mouseForce
+                        self.yforce = self.mass * self.yVelocityDelayed - self.yVelocity
                     if self.yLocation > screenHeight - self.yLength:
                         self.yLocation = 2 * (screenHeight - self.yLength) - self.yLocation
                         self.yVelocity *= self.elasticity
@@ -125,11 +136,11 @@ def collide(i1,i2):
             i1.xVelocity *= i1.drag
             i2.xVelocity *= i2.drag
             if i1.yVelocity < 0:
-                i1.yLocation += 1
-                i2.yLocation -= 1
-            else:
                 i1.yLocation -= 1
                 i2.yLocation += 1
+            else:
+                i1.yLocation += 1
+                i2.yLocation -= 1
             if i1.xVelocity < 0:
                 i1.xLocation -= 1
                 i2.xLocation += 1
@@ -148,6 +159,8 @@ canvas.fill([255,255,255])
 time = pygame.time.Clock()
 rateOfTime = 1
 run = True
+selection = None
+totalforce = 0
 while run:
     time.tick(rateOfTime*120)
     canvas.fill([255,255,255])
@@ -160,14 +173,24 @@ while run:
             for instance in instances:
                 if clickbox.colliderect(instances[instance].visual):
                     instances[instance].selected = True
+                    selection = instances[instance]
                     break
         if event.type == pygame.MOUSEBUTTONUP:
             for instance in instances:
                 if instances[instance].selected:
                     instances[instance].selected = False
+                    selection = None
         if event.type == pygame.MOUSEMOTION:
             xM = event.rel[0]
             yM = event.rel[1]
+    try:
+        pygame.draw.aaline(canvas, [255,0,0], pygame.mouse.get_pos(), [selection.xLocation+(selection.xLength/2),selection.yLocation+(selection.yLength/2)])
+        totalforce = round(math.hypot(selection.xforce/scale,selection.yforce/scale),2)
+        text = fonty.render(str(totalforce)+"N",False,[0,0,0])
+        canvas.blit(text,(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]+10))
+    except:
+        text = fonty.render(str(totalforce)+"N",True,[0,0,0])
+        canvas.blit(text,(pygame.mouse.get_pos()[0]+5,pygame.mouse.get_pos()[1]+10))
     for item in instances:
         for item2 in instances:
             if instances[item] != instances[item2]:
